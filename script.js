dc687ab8c49980fed320682b3c2161ddadaa8d3b
Lisha Jins <lishajins2006@gmail.com>
2026-06-07 09:18:12 +0530

    basic console game script

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("hi")
+function getComputerChoice(){
+    let x= Math.random();
+    let cChoice;
+    if(x<0.3){
+        cChoice="Rock";
+    }else if(x>=0.3 && x<0.7){
+        cChoice="Paper";
+    }else{
+        cChoice="Scissors";
+    }
+    return cChoice;
+}
+
+function getHumanChoice(){
+    let hChoice=prompt("Enter your choice");
+    return hChoice;
+}
+let humanScore=0;
+let computerScore=0;
+
+function playRound(hChoice,cChoice){
+    let human=hChoice.toLowerCase();
+    let computer=cChoice.toLowerCase();
+
+    if ( human === computer){
+        console.log("Its a tie!");
+    }
+    else{
+        if(human === "rock" && computer === "scissors"){
+            console.log(`Human wins! ${human} beats ${computer}.`);
+            humanScore++;
+        }
+        else if(human === "paper" && computer === "rock"){
+            console.log(`Human wins! ${human} beats ${computer}.`);
+            humanScore++;
+        }
+        else if(human === "scissors" && computer === "paper"){
+            console.log(`Human wins! ${human} beats ${computer}.`);
+            humanScore++;
+        }else{
+            console.log(`Computer wins! ${computer} beats ${human}.`);
+            computerScore++;
+        }
+    }
+}
+
+function playGame(){
+    for(let i=0;i<5;i++){
+        let x=getComputerChoice();
+        let y=getHumanChoice();
+
+        playRound(y,x);
+    }
+}
+
+playGame();
